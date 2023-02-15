dcc6f4dbf620336bbe27766e33d573cfd578f319
kiril igorevich reshetnikov <reshetnikov@bright90.cm.cluster>
2023-02-15 17:30:22 +0500

    update

--- labelutilits/_data_spliter.py
+++ labelutilits/_data_spliter.py
@@ -7,9 +7,9 @@ import shutil
 from labelutilits._path import _cp_file_list
 
 
-def k_fold_split_yolo(path2label,
-                      path2image,
-                      path_save_fold,
+def k_fold_split_yolo(path2label:str,
+                      path2image:str,
+                      path_save_fold:str,
                       number_fold: int = 4):
 
     l_labels = sorted(list_ext(path2label), key = lambda x: x.split('.')[0])
@@ -29,7 +29,7 @@ def k_fold_split_yolo(path2label,
         path_2_fold.mkdir()
         
         train_images = [path2image / name for name in list(map(l_images.__getitem__, train_indxs))]
-        _cp_file_list(path_2_fold,"train/", train_images);
+        _cp_file_list(path_2_fold,"train/", train_images);g
         
         test_images = [path2image / name for name in list(map(l_images.__getitem__, test_indxs))]
         _cp_file_list(path_2_fold,"test/", test_images);
